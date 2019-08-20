f4aeb01dfb1c250c33485981f530b39e3f03b105
Alex Van Camp <vancamp.alex@gmail.com>
2019-08-20 16:02:01 -0500

    reduce

--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,3 @@
 import * as svgjs from "@svgdotjs/svg.js";
-import "@svgdotjs/svg.filter.js";
+import "@svgdotjs/svg.filter.js"; // this is the line that errors
 console.log(Object.keys(svgjs).length);
